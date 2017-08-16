48c9c9e06b9134d3b85466fd4b90dce684e95494
N2ET <1766871551@qq.com>
2017-08-16 20:44:04 +0800

    init another version form

---
+++ packages/form-1/src/form.vue
@@ -0,0 +1,96 @@
+<template>
+    <form class="el-form" :class="[
+        labelPosition ? 'el-form--label-' + labelPosition : '',
+        { 'el-form--inline': inline }
+      ]">
+        <slot></slot>
+    </form>
+</template>
+<script>
+export default {
+    name: 'ElForm',
+
+    componentName: 'ElForm',
+
+    props: {
+        model: Object,
+        rules: Object,
+        labelPosition: String,
+        labelWidth: String,
+        labelSuffix: {
+            type: String,
+            default: ''
+        },
+        inline: Boolean
+        // showMessage: {
+        //     type: Boolean,
+        //     default: true
+        // }
+    },
+    watch: {
+        // rules() {
+        //     this.validate();
+        // }
+    },
+    data() {
+        return {
+            fields: [],
+            rules: {
+                '#id': {
+                    minValue: 100,
+                    maxValue: 1000,
+                    validator () {},
+                    cb () {}
+                }
+            }
+        };
+    },
+    created() {
+        this.$on('el.form.addField', (field) => {
+            if (field) {
+                this.fields.push(field);
+            }
+        });
+        /* istanbul ignore next */
+        this.$on('el.form.removeField', (field) => {
+            if (field.prop) {
+                this.fields.splice(this.fields.indexOf(field), 1);
+            }
+        });
+    },
+    methods: {
+        resetFields() {
+            if (!this.model) {
+                process.env.NODE_ENV !== 'production' &&
+                    console.warn('[Element Warn][Form]model is required for resetFields to work.');
+                return;
+            }
+            this.fields.forEach(field => {
+                field.resetField();
+            });
+        },
+
+        validate(callback) {
+            
+        },
+
+        validateField(prop, cb) {
+            
+        },
+
+        getRule() {
+
+        },
+
+        addRule() {
+
+        },
+
+        removeRule() {
+
+        }
+
+
+    }
+};
+</script>
